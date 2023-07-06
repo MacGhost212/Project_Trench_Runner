62cda94d82d86aea2b9caf4c10a0a24fe7624947
L00151915 <conormac09@aol.com>
2023-07-06 15:00:03 +0100

    VR WW1 Game

---
+++ TrenchRunner4.0/Assets/Scripts/UIFollow3DObject.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFollow3DObject : MonoBehaviour
+
+{
+    public Transform mainCam;
+    public Transform target;
+    public Transform worldSpaceCanvas;
+    public Vector3 offset;
+
+
+    private void Start()
+    {
+        mainCam = Camera.main.transform;
+        transform.SetParent(worldSpaceCanvas);
+
+    }
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - mainCam.transform.position); // look at camera
+
+        transform.position = target.position + offset;
+
+    }
+}
